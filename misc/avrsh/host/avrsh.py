d8c52dfd40c4f8ba58f8b02e8f7d529215a93084
tickelton <tickelton@gmail.com>
2020-09-19 11:52:19 +0200

    Turns avrsh.py into a proper shell client
    avrsh.py previously only offered the possibility to send
    a single command per invocation to the client device.
    It now has a proper read loop that makes it more like
    the terminal client one would expect as it lets one
    send an arbitrary number of successive commands.

--- misc/avrsh/host/avrsh.py
+++ misc/avrsh/host/avrsh.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+'''
+Python client for the avrsh firmware on the AVR Stick
+
+This scripts acts as the client terminal application for
+interfacing with avrsh on an AVR Stick over HID feature
+reports.
+
+Example usage:
+
+    $ sudo python avrsh.py
+    > fcpu
+    CPU Freq: 16500000
+    > set pin PB3 output
+    Set PB3 for output
+    > write pin PB3 high
+    Wrote logic high to pin PB3
+    > write pin PB3 low
+    Wrote logic low to pin PB3
+
+'''
 
 # Copyright (c) 2020 tick <tickelton@gmail.com>
 # SPDX-License-Identifier: ISC
@@ -10,10 +30,6 @@ from usb1 import REQUEST_TYPE_CLASS, RECIPIENT_DEVICE, ENDPOINT_IN, ENDPOINT_OUT
 
 vendor_id = 0x16c0
 product_id = 0x05df
-
-def usage(name):
-    print('usage:')
-    print('  %s DATA' % name)
 
 
 def find_device(ctx, vendor, product):
@@ -38,30 +54,35 @@ if __name__ == '__main__':
                 print('Something went wrong')
                 sys.exit(1)
 
-            if len(sys.argv) > 1:
-                buf = ' '.join(sys.argv[1:]).encode()
-                #print('send: buf = "%s" len = %d', (buf, len(buf)))
-                sent = handle.controlWrite(
-                    REQUEST_TYPE_CLASS | RECIPIENT_DEVICE | ENDPOINT_OUT,
-                    9,
-                    3<<8 | 0,
-                    0,
-                    buf,
-                    5000
-                )
-                if sent != len(buf):
-                    print('Warning: %d bytes sent, expected %d' % (sent, len(buf)))
+            while True:
+                try:
+                    cmd = input('> ')
+                    if len(cmd) < 1:
+                        continue
+                    cmdlist = cmd.split(' ')
 
-                while True:
-                    buf = handle.controlRead(REQUEST_TYPE_CLASS | RECIPIENT_DEVICE | ENDPOINT_IN, 1, 3<<8 | 0, 0, 32, 2000)
-                    #print('recv: buf = "%s" len = %d', (buf, len(buf)))
-                    if len(buf) == 0:
-                        break
-                    else:
-                        print('Received: "%s"' % buf.decode())
-            else:
-                usage(os.path.basename(sys.argv[0]))
-                ret = 1
+                    #print('send: cmd = "%s" len = %d', (cmd.encode(), len(cmd)))
+                    sent = handle.controlWrite(
+                        REQUEST_TYPE_CLASS | RECIPIENT_DEVICE | ENDPOINT_OUT,
+                        9,
+                        3<<8 | 0,
+                        0,
+                        cmd.encode(),
+                        5000
+                    )
+                    if sent != len(cmd):
+                        print('Warning: %d bytes sent, expected %d' % (sent, len(cmd)))
+
+                    while True:
+                        buf = handle.controlRead(REQUEST_TYPE_CLASS | RECIPIENT_DEVICE | ENDPOINT_IN, 1, 3<<8 | 0, 0, 32, 2000)
+                        #print('recv: buf = "%s" len = %d', (buf, len(buf)))
+                        if len(buf) == 0:
+                            break
+                        else:
+                            print(buf.decode(), end='')
+                except (KeyboardInterrupt, EOFError):
+                    break;
+
             handle.close()
 
     sys.exit(ret)
